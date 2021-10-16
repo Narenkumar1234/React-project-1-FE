cd0da00a57461e8b805b995fc13e28fc89318014
Narenkumar1234 <narenkumar2001@gmail.com>
2021-10-16 21:11:15 +0530

    Add Battery Camera Update Filter

--- src/Components/App.js
+++ src/Components/App.js
@@ -11,9 +11,12 @@ return (
     <>
     <Router>
     <Switch history={history} >
-        <Route  path="/" exact component={Home}></Route>
-        <Route  path="/getMobilePhone/:id" exact component={withRouter(Filter)}></Route>
-        <Route  path="/getMobilePhone/:id/:price" exact component={withRouter(Filter)}></Route>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/getMobilePhone/:id" exact component={withRouter(Filter)}></Route>
+        <Route path="/getMobilePhone/:id/:price" exact component={withRouter(Filter)}></Route>
+        <Route path="/getMobilePhone/:id/:price/:batt" exact component ={withRouter(Filter)}></Route>
+        <Route path="/getMobilePhone/:id/:price/:batt/:cam" exact component ={withRouter(Filter)}></Route>
+        <Route path="/getMobilePhone/:id/:price/:batt/:cam/:disp" exact component ={withRouter(Filter)}></Route>
     </Switch>   
    </Router>
     </>
